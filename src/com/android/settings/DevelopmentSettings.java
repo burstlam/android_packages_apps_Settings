d96dae85eac90546e838b485fc52ab06d239357b
blunden <blunden2@gmail.com>
2012-05-30 21:59:31 +0200

    Settings: Use ICS alert drawable
    Patchset 2: Removed extra period
    
    Change-Id: I491e7816caf0757adf23e3712e382e3838f2abd5
    
    Conflicts:
    
    	src/com/android/settings/DevelopmentSettings.java
    	src/com/android/settings/cmstats/AnonymousStats.java
    	src/com/android/settings/cyanogenmod/LockscreenTargets.java
    	src/com/android/settings/cyanogenmod/NavBar.java
    	src/com/android/settings/notificationlight/NotificationLightSettings.java
    	src/com/android/settings/profiles/AppGroupConfig.java
    	src/com/android/settings/profiles/ProfilesSettings.java

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -431,7 +431,7 @@ public class DevelopmentSettings extends PreferenceFragment
                 mOkDialog = new AlertDialog.Builder(getActivity()).setMessage(
                         getActivity().getResources().getString(R.string.adb_warning_message))
                         .setTitle(R.string.adb_warning_title)
-                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setIconAttribute(android.R.attr.alertDialogIcon)
                         .setPositiveButton(android.R.string.yes, this)
                         .setNegativeButton(android.R.string.no, this)
                         .show();
@@ -448,7 +448,7 @@ public class DevelopmentSettings extends PreferenceFragment
                 mOkDialog = new AlertDialog.Builder(getActivity()).setMessage(
                     getResources().getString(R.string.adb_over_network_warning))
                     .setTitle(R.string.adb_over_network)
-                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .setIconAttribute(android.R.attr.alertDialogIcon)
                     .setPositiveButton(android.R.string.yes, this)
                     .setNegativeButton(android.R.string.no, this)
                     .show();
@@ -503,7 +503,7 @@ public class DevelopmentSettings extends PreferenceFragment
         } else if (preference == mAppProcessLimit) {
             writeAppProcessLimitOptions(newValue);
             return true;
-        }
+       }
         return false;
     }
 
