f9658d3dd6166ef027d7ed13d8bfb34d9e29fdb1
PeterCxy <xqsx43cxy@126.com>
2013-07-31 10:34:24 +0800

    Fix build
    Change-Id: I77091b04fd5e20ee71e528ac85759a803ab3be77

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -441,7 +441,7 @@ public class Settings extends PreferenceActivity
             // Ids are integers, so downcasting
             int id = (int) header.id;
             if (id == R.id.operator_settings || id == R.id.manufacturer_settings ||
-                    id == R.id.advanced_settings || id == R.id.slim_center) {
+                    id == R.id.advanced_settings) {
                 Utils.updateHeaderToSpecificActivityFromMetaDataOrRemove(this, target, header);
             } else if (id == R.id.wifi_settings) {
                 // Remove WiFi Settings if WiFi service is not available.
