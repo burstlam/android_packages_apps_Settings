2f613e06cb905ae471d28a0a865e91041ecbb552
burstlam <burstlam@gmail.com>
2013-10-09 13:46:35 +0800

    Gesture Anywhere : Fix Compile

--- src/com/android/settings/chameleonos/labs/gestureanywhere/GestureAnywhereSettings.java
+++ src/com/android/settings/chameleonos/labs/gestureanywhere/GestureAnywhereSettings.java
@@ -28,7 +28,7 @@ import android.provider.Settings;
 import android.view.Gravity;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.chameleonos.SeekBarPreference;
+import com.android.settings.slim.SeekBarPreference;
 
 public class GestureAnywhereSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
