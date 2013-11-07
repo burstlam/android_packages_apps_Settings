31cd700575e90ab5871078b59c4bf87d95922e44
burstlam <burstlam@gmail.com>
2013-11-07 20:37:43 +0800

    Bridge to Performance Control

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -939,4 +939,5 @@ public class Settings extends PreferenceActivity
     public static class UserSettingsActivity extends Settings { /* empty */ }
     public static class NotificationAccessSettingsActivity extends Settings { /* empty */ }
     public static class BlacklistSettingsActivity extends Settings { /* empty */ }
+    public static class PerformanceSettingsActivity extends Settings { /* empty */ }
 }
