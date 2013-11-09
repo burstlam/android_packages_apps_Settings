db8faa25c8e5ed25f7554cb6f34ff342101d3733
Lars Greiss <kufikugel@googlemail.com>
2013-11-09 05:04:13 +0100

    Settings: Slims ElectronBeam get some love (2/2)
    Features:
    - You can select different crt off animation modes if device overlay does not disable it at all
    - disabled
    - always horizontal (stock behaviour)
    - always vertical (screen collapse from left and right to the middle)
    - orientation dependent (means portrait = horizontal animation, landscape = vertical animation)
    - some minor fixes/code style improvements to 4.3 implementation
    
    Change-Id: I35fb277b8da823d50cc4db1eec32ea597fe973f2
    
    Conflicts:
    	res/values/slim_arrays.xml
    	res/xml/display_settings.xml
    	src/com/android/settings/DisplaySettings.java

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -69,6 +69,8 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private static final String KEY_NOTIFICATION_PULSE = "notification_pulse";
     private static final String KEY_BATTERY_LIGHT = "battery_light";
     private static final String KEY_WAKE_WHEN_PLUGGED_OR_UNPLUGGED = "wake_when_plugged_or_unplugged";
+    private static final String KEY_ANIMATION_OPTIONS = "category_animation_options";
+    private static final String KEY_POWER_CRT_MODE = "system_power_crt_mode";
 
     private static final int DLG_GLOBAL_CHANGE_WARNING = 1;
 
@@ -84,7 +86,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
 
     private ListPreference mScreenTimeoutPreference;
     private Preference mScreenSaverPreference;
-
+    private ListPreference mCrtMode;
     private CheckBoxPreference mAdaptiveBacklight;
 
     private ContentObserver mAccelerometerRotationObserver =
@@ -167,6 +169,23 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
             }
         } else {
             getPreferenceScreen().removePreference(lightPrefs);
+        }
+
+        // respect device default configuration
+        // true fades while false animates
+        boolean electronBeamFadesConfig = getResources().getBoolean(
+                com.android.internal.R.bool.config_animateScreenLights);
+        PreferenceCategory animationOptions =
+            (PreferenceCategory) findPreference(KEY_ANIMATION_OPTIONS);
+        mCrtMode = (ListPreference) findPreference(KEY_POWER_CRT_MODE);
+        if (!electronBeamFadesConfig && mCrtMode != null) {
+            int crtMode = Settings.System.getInt(getContentResolver(),
+                    Settings.System.SYSTEM_POWER_CRT_MODE, 1);
+            mCrtMode.setValue(String.valueOf(crtMode));
+            mCrtMode.setSummary(mCrtMode.getEntry());
+            mCrtMode.setOnPreferenceChangeListener(this);
+        } else if (animationOptions != null) {
+            getPreferenceScreen().removePreference(animationOptions);
         }
     }
 
@@ -429,7 +448,14 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         if (KEY_FONT_SIZE.equals(key)) {
             writeFontSizePreference(objValue);
         }
-
+        if (KEY_POWER_CRT_MODE.equals(key)) {
+            int value = Integer.parseInt((String) objValue);
+            int index = mCrtMode.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.SYSTEM_POWER_CRT_MODE,
+                    value);
+            mCrtMode.setSummary(mCrtMode.getEntries()[index]);
+        }
         return true;
     }
 
