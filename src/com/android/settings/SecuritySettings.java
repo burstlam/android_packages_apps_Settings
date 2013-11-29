6e79ec8d93a55c3f942765e0e24554b3b3766974
Lars Greiss <kufikugel@googlemail.com>
2013-11-29 04:41:59 +0100

    Settings: Lock before unlock the new way (2/2)
    for more information see fb commit please.
    target selection if lock before unlock is activated
    will be enabled and shows the repective settings dependend
    on if it is a phone or tablet
    
    Change-Id: I6bb41539383c2b02b62354b00c773505f396028e
    
    Conflicts:
    	res/values/slim_strings.xml
    	res/xml/security_settings_biometric_weak.xml
    	res/xml/security_settings_password.xml
    	res/xml/security_settings_pin.xml
    	src/com/android/settings/SecuritySettings.java

--- src/com/android/settings/SecuritySettings.java
+++ src/com/android/settings/SecuritySettings.java
@@ -70,6 +70,7 @@ public class SecuritySettings extends RestrictedSettingsFragment
     private static final String KEY_LOCK_AFTER_TIMEOUT = "lock_after_timeout";
     private static final String KEY_OWNER_INFO_SETTINGS = "owner_info_settings";
     private static final String LOCK_NUMPAD_RANDOM = "lock_numpad_random";
+    private static final String LOCK_BEFORE_UNLOCK = "lock_before_unlock";
 
     private static final int SET_OR_CHANGE_LOCK_METHOD_REQUEST = 123;
     private static final int CONFIRM_EXISTING_FOR_BIOMETRIC_WEAK_IMPROVE_REQUEST = 124;
@@ -100,6 +101,7 @@ public class SecuritySettings extends RestrictedSettingsFragment
     private LockPatternUtils mLockPatternUtils;
     private ListPreference mLockAfter;
     private ListPreference mLockNumpadRandom;
+    private CheckBoxPreference mLockBeforeUnlock;
 
     private CheckBoxPreference mBiometricWeakLiveliness;
     private CheckBoxPreference mVisiblePattern;
@@ -256,6 +258,16 @@ public class SecuritySettings extends RestrictedSettingsFragment
                     Settings.Secure.LOCK_NUMPAD_RANDOM, 0)));
             mLockNumpadRandom.setSummary(mLockNumpadRandom.getEntry());
             mLockNumpadRandom.setOnPreferenceChangeListener(this);
+        }
+
+        // Lock before Unlock
+        mLockBeforeUnlock = (CheckBoxPreference) root
+                .findPreference(LOCK_BEFORE_UNLOCK);
+        if (mLockBeforeUnlock != null) {
+            mLockBeforeUnlock.setChecked(
+                    Settings.Secure.getInt(getContentResolver(),
+                    Settings.Secure.LOCK_BEFORE_UNLOCK, 0) == 1);
+            mLockBeforeUnlock.setOnPreferenceChangeListener(this);
         }
 
         // Append the rest of the settings
@@ -664,6 +676,10 @@ public class SecuritySettings extends RestrictedSettingsFragment
             Settings.Global.putInt(getContentResolver(),
                     Settings.Global.SMS_OUTGOING_CHECK_MAX_COUNT, smsSecurityCheck);
             updateSmsSecuritySummary(smsSecurityCheck);
+        } else if (preference == mLockBeforeUnlock) {
+            Settings.Secure.putInt(getContentResolver(),
+                    Settings.Secure.LOCK_BEFORE_UNLOCK,
+                    ((Boolean) value) ? 1 : 0);
         }
         return true;
     }
