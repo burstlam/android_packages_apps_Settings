0a57bf8476a9f5c881b9679fe317f65fa0c8e600
Lars Greiss <kufikugel@googlemail.com>
2013-12-03 09:44:17 +0100

    Quicksettings customizations Slim style
    First thank to CM for the general base idea.....but we changed, fixed a lot
    as well the UX is not the best on original implementation. It is way to much
    to list all down what we changed and ehanced and fixed...so just a small overview
    
    Whatever this patch adds following tiles:
    
    Airplane mode
    Battery
    Bluetooth
    Brightness
    Go to sleep
    Location
    Torch
    Lock screen
    LTE
    Mobile data
    Orientation
    Settings
    Sound
    Sync
    Wi-Fi AP
    Wi-Fi
    User switcher
    Network mode
    NFC
    Screen timeout
    Quiet hours
    Volume
    Expanded desktop
    Music - new nifty one ;)
    Reboot
    
    and the usual dynamic tiles which are always dynamic now. As well we put all setup
    options into one screen.
    
    as reference see
    
    http://s1.directupload.net/file/d/3461/phhimlo7_png.htm
    http://s7.directupload.net/file/d/3461/fvaxgins_png.htm
    
    and video
    http://www.file-upload.net/download-8362155/drawer.mp4.html
    
    PS4:
    - remove drag on longpress and drag directly
    - remove scalable and change alpha a bit
    PS5:
    - remove complete scaling and keep tile size
    - small tweeks in animation
    PS6
    - add style options
    PS7
    - revert back to longpress on setup drawer
    - reflect tiles per row and landscape behaviour as well in settings screen
    - minor fixes and tweeks
    PS8:
    - reflect the tile view exactly like it is setuped in fb
    - show popup on tiles which have additional settings
    - add for this popup indicator
    - animation fixes
    - cleanup
    PS9:
    - do the settings a bit nicer and logical
    PS11:
    - added  quiethours pseudo activity
    
    cleanuped and finished
    
    Change-Id: I6b1a81430056772d70e4f34c2c54916e7aa146a6
    
    Conflicts:
    	AndroidManifest.xml

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -1148,4 +1148,5 @@ public class Settings extends PreferenceActivity
     public static class ApnSettingsActivity extends Settings { /* empty */ }
     public static class ApnEditorActivity extends Settings { /* empty */ }
     public static class BlacklistSettingsActivity extends Settings { /* empty */ }
+    public static class QuietHoursSettingsActivity extends Settings { /* empty */ }
 }
