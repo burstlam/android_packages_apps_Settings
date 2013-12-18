SlimRoms/packages_apps_Settings

settings: allow to disable quicksettings
on 4.3 we had an extra settings option for it. Due that we want
to reduce the settings to improve the UX and due that
we are doing it this way on all dslv settings we change
the old 4.3 way.

Now it is very easy like dslv.....remove all tiles and QS
is completly disabled and cannot be opend any more. Notification
drawer shows the settings icon on the top right in this case.
As well compared to 4.3 this works on phones and tablets

Code cleanup and follow AOSP code style guidelines.

Some minor dialog tweeks (help dialog etc)

Change-Id: Ic3007e3cff0a093b29c28f32ffc0d0951a771b93

src/com/android/settings/slim/NotificationDrawerQsSettings.java
@@ -25,6 +25,7 @@ import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
 import android.provider.Settings;
+import android.os.UserHandle;
 
 import com.android.internal.util.slim.DeviceUtils;
 
@@ -42,7 +43,12 @@ public class NotificationDrawerQsSettings extends SettingsPreferenceFragment
             "notification_hide_carrier";
     private static final String PREF_NOTIFICATION_ALPHA =
             "notification_alpha";
-    private static final String QUICK_PULLDOWN = "quick_pulldown";
+    private static final String PRE_QUICK_PULLDOWN =
+            "quick_pulldown";
+    private static final String PREF_TILES_STYLE =
+            "quicksettings_tiles_style";
+    private static final String PREF_TILE_PICKER =
+            "tile_picker";
 
     CheckBoxPreference mHideCarrier;
     SeekBarPreference mNotificationAlpha;
@@ -86,7 +92,7 @@ public class NotificationDrawerQsSettings extends SettingsPreferenceFragment
         mNotificationAlpha.setProperty(Settings.System.NOTIFICATION_ALPHA);
         mNotificationAlpha.setOnPreferenceChangeListener(this);
 
-        mQuickPulldown = (ListPreference) findPreference(QUICK_PULLDOWN);
+        mQuickPulldown = (ListPreference) findPreference(PRE_QUICK_PULLDOWN);
         if (!DeviceUtils.isPhone(getActivity())) {
             prefs.removePreference(mQuickPulldown);
         } else {
@@ -96,12 +102,30 @@ public class NotificationDrawerQsSettings extends SettingsPreferenceFragment
             mQuickPulldown.setValue(String.valueOf(statusQuickPulldown));
             updatePulldownSummary();
         }
+
+        updateQuickSettingsOptions();
+    }
+
+    private void updateQuickSettingsOptions() {
+        Preference tilesStyle = (Preference) findPreference(PREF_TILES_STYLE);
+        Preference tilesPicker = (Preference) findPreference(PREF_TILE_PICKER);
+        String qsConfig = Settings.System.getStringForUser(getContentResolver(),
+                Settings.System.QUICK_SETTINGS_TILES, UserHandle.USER_CURRENT);
+        boolean hideSettingsPanel = qsConfig != null && qsConfig.isEmpty();
+        mQuickPulldown.setEnabled(!hideSettingsPanel);
+        tilesStyle.setEnabled(!hideSettingsPanel);
+        if (hideSettingsPanel) {
+            tilesPicker.setSummary(getResources().getString(R.string.disable_qs));
+        } else {
+            tilesPicker.setSummary(getResources().getString(R.string.tile_picker_summary));
+        }
     }
 
     @Override
     public void onResume() {
         super.onResume();
         QuickSettingsUtil.updateAvailableTiles(getActivity());
+        updateQuickSettingsOptions();
     }
 
     @Override

burstlam/android_packages_apps_Settings

Conflicts:
	src/com/android/settings/slim/NotificationDrawerQsSettings.java

src/com/android/settings/slim/NotificationDrawerQsSettings.java
@@ -25,6 +25,7 @@ import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
 import android.provider.Settings;
+import android.os.UserHandle;
 
 import com.android.internal.util.slim.DeviceUtils;
 
@@ -42,7 +43,12 @@ public class NotificationDrawerQsSettings extends SettingsPreferenceFragment
             "notification_hide_carrier";
     private static final String PREF_NOTIFICATION_ALPHA =
             "notification_alpha";
-    private static final String QUICK_PULLDOWN = "quick_pulldown";
+    private static final String QUICK_PULLDOWN =
+            "quick_pulldown";
+    private static final String PREF_TILES_STYLE =
+            "quicksettings_tiles_style";
+    private static final String PREF_TILE_PICKER =
+            "tile_picker";
 
     private static final String KEY_NOTIFICATION_DRAWER = "notification_drawer";
     private static final String KEY_NOTIFICATION_DRAWER_TABLET = "notification_drawer_tablet";
@@ -92,7 +98,7 @@ public class NotificationDrawerQsSettings extends SettingsPreferenceFragment
         mNotificationAlpha.setProperty(Settings.System.NOTIFICATION_ALPHA);
         mNotificationAlpha.setOnPreferenceChangeListener(this);
 
-	mPhoneDrawer = (PreferenceScreen) findPreference(KEY_NOTIFICATION_DRAWER);
+    	mPhoneDrawer = (PreferenceScreen) findPreference(KEY_NOTIFICATION_DRAWER);
         mTabletDrawer = (PreferenceScreen) findPreference(KEY_NOTIFICATION_DRAWER_TABLET);
 
         /*if (Utils.isTablet(getActivity())) {
@@ -105,7 +111,7 @@ public class NotificationDrawerQsSettings extends SettingsPreferenceFragment
             }
         }
 
-        mQuickPulldown = (ListPreference) findPreference(QUICK_PULLDOWN);
+        mQuickPulldown = (ListPreference) findPreference(PRE_QUICK_PULLDOWN);
         if (!DeviceUtils.isPhone(getActivity())) {
             prefs.removePreference(mQuickPulldown);
         } else {
@@ -115,12 +121,30 @@ public class NotificationDrawerQsSettings extends SettingsPreferenceFragment
             mQuickPulldown.setValue(String.valueOf(statusQuickPulldown));
             updatePulldownSummary();
         }
+
+        updateQuickSettingsOptions();
+    }
+
+    private void updateQuickSettingsOptions() {
+        Preference tilesStyle = (Preference) findPreference(PREF_TILES_STYLE);
+        Preference tilesPicker = (Preference) findPreference(PREF_TILE_PICKER);
+        String qsConfig = Settings.System.getStringForUser(getContentResolver(),
+                Settings.System.QUICK_SETTINGS_TILES, UserHandle.USER_CURRENT);
+        boolean hideSettingsPanel = qsConfig != null && qsConfig.isEmpty();
+        mQuickPulldown.setEnabled(!hideSettingsPanel);
+        tilesStyle.setEnabled(!hideSettingsPanel);
+        if (hideSettingsPanel) {
+            tilesPicker.setSummary(getResources().getString(R.string.disable_qs));
+        } else {
+            tilesPicker.setSummary(getResources().getString(R.string.tile_picker_summary));
+        }
     }
 
     @Override
     public void onResume() {
         super.onResume();
         QuickSettingsUtil.updateAvailableTiles(getActivity());
+        updateQuickSettingsOptions();
     }
 
     @Override