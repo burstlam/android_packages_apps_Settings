2615ed3440bbd9545d77e1c951101d249b6dd4a7
Muhammed Nazim <mnm9994u@gmail.com>
2014-01-19 10:19:05 +0100

    Return: Smart Pulldown [2/2]
    Adds the ability to directly open the QS panel when there are
    no notifications present in the notification panel.
    This commit combines the enhancement by HardCorePawn for detectin
    notification types for the same.
    
    Credits: kufikugel, HardCorePawn
    
    Change-Id: I0a6526f8367bdf86a5216c5be0f140c80b1386d9

--- src/com/android/settings/slim/NotificationDrawerQsSettings.java
+++ src/com/android/settings/slim/NotificationDrawerQsSettings.java
@@ -46,6 +46,8 @@ public class NotificationDrawerQsSettings extends SettingsPreferenceFragment
             "notification_alpha";
     private static final String PRE_QUICK_PULLDOWN =
             "quick_pulldown";
+    private static final String PRE_SMART_PULLDOWN =
+            "smart_pulldown";
     private static final String PRE_COLLAPSE_PANEL =
             "collapse_panel";
     private static final String PREF_TILES_STYLE =
@@ -62,6 +64,7 @@ public class NotificationDrawerQsSettings extends SettingsPreferenceFragment
     ListPreference mHideLabels;
     SeekBarPreference mNotificationAlpha;
     ListPreference mQuickPulldown;
+    ListPreference mSmartPulldown;
     CheckBoxPreference mCollapsePanel;
 
     @Override
@@ -117,14 +120,24 @@ public class NotificationDrawerQsSettings extends SettingsPreferenceFragment
         }
 
         mQuickPulldown = (ListPreference) findPreference(PRE_QUICK_PULLDOWN);
+        mSmartPulldown = (ListPreference) findPreference(PRE_SMART_PULLDOWN);
         if (!DeviceUtils.isPhone(getActivity())) {
             prefs.removePreference(mQuickPulldown);
+            prefs.removePreference(mSmartPulldown);
         } else {
+            // Quick Pulldown
             mQuickPulldown.setOnPreferenceChangeListener(this);
             int statusQuickPulldown = Settings.System.getInt(getContentResolver(),
                     Settings.System.QS_QUICK_PULLDOWN, 0);
             mQuickPulldown.setValue(String.valueOf(statusQuickPulldown));
-            updatePulldownSummary(statusQuickPulldown);
+            updateQuickPulldownSummary(statusQuickPulldown);
+
+            // Smart Pulldown
+            mSmartPulldown.setOnPreferenceChangeListener(this);
+            int smartPulldown = Settings.System.getIntForUser(getContentResolver(),
+                    Settings.System.QS_SMART_PULLDOWN, 0, UserHandle.USER_CURRENT);
+            mSmartPulldown.setValue(String.valueOf(smartPulldown));
+            updateSmartPulldownSummary(smartPulldown);
         }
 
         mCollapsePanel = (CheckBoxPreference) findPreference(PRE_COLLAPSE_PANEL);
@@ -142,6 +155,7 @@ public class NotificationDrawerQsSettings extends SettingsPreferenceFragment
                 Settings.System.QUICK_SETTINGS_TILES, UserHandle.USER_CURRENT);
         boolean hideSettingsPanel = qsConfig != null && qsConfig.isEmpty();
         mQuickPulldown.setEnabled(!hideSettingsPanel);
+        mSmartPulldown.setEnabled(!hideSettingsPanel);
         tilesStyle.setEnabled(!hideSettingsPanel);
         if (hideSettingsPanel) {
             tilesPicker.setSummary(getResources().getString(R.string.disable_qs));
@@ -174,7 +188,13 @@ public class NotificationDrawerQsSettings extends SettingsPreferenceFragment
             int statusQuickPulldown = Integer.valueOf((String) newValue);
             Settings.System.putInt(getContentResolver(), Settings.System.QS_QUICK_PULLDOWN,
                     statusQuickPulldown);
-            updatePulldownSummary(statusQuickPulldown);
+            updateQuickPulldownSummary(statusQuickPulldown);
+            return true;
+        } else if (preference == mSmartPulldown) {
+            int smartPulldown = Integer.valueOf((String) newValue);
+            Settings.System.putInt(getContentResolver(), Settings.System.QS_SMART_PULLDOWN,
+                    smartPulldown);
+            updateSmartPulldownSummary(smartPulldown);
             return true;
         } else if (preference == mCollapsePanel) {
             Settings.System.putInt(getContentResolver(),
@@ -185,7 +205,7 @@ public class NotificationDrawerQsSettings extends SettingsPreferenceFragment
         return false;
     }
 
-    private void updatePulldownSummary(int value) {
+    private void updateQuickPulldownSummary(int value) {
         Resources res = getResources();
 
         if (value == 0) {
@@ -196,6 +216,20 @@ public class NotificationDrawerQsSettings extends SettingsPreferenceFragment
                     ? R.string.quick_pulldown_left
                     : R.string.quick_pulldown_right);
             mQuickPulldown.setSummary(res.getString(R.string.summary_quick_pulldown, direction));
+        }
+    }
+
+    private void updateSmartPulldownSummary(int value) {
+        Resources res = getResources();
+
+        if (value == 0) {
+            // Smart pulldown deactivated
+            mSmartPulldown.setSummary(res.getString(R.string.smart_pulldown_off));
+        } else {
+            String type = res.getString(value == 2
+                    ? R.string.smart_pulldown_persistent
+                    : R.string.smart_pulldown_dismissable);
+            mSmartPulldown.setSummary(res.getString(R.string.smart_pulldown_summary, type));
         }
     }
 
