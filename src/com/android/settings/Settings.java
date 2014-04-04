957d66011502c464a3a5b29613261cea21cc3d04
burstlam <burstlam@gmail.com>
2014-04-04 14:50:11 +0800

    Settings : multiuser support

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -180,9 +180,15 @@ public class Settings extends PreferenceActivity
             R.id.nfc_payment_settings,
             R.id.home_settings,
             R.id.interface_section,
-            //R.id.lock_screen_settings,
+            R.id.lockscreen_settings,
             //R.id.system_settings,
             R.id.privacy_settings_cyanogenmod,
+            R.id.interface_settings,
+            R.id.navigation_settings,
+            R.id.button_settings,
+            R.id.lab_settings,
+            R.id.installer,
+            R.id.advanced_settings,
     };
 
     private SharedPreferences mDevelopmentPreferences;
