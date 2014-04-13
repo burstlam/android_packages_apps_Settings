16851af26d21c6fcb2209cd4d703bb78f89653a2
burstlam <burstlam@gmail.com>
2014-04-13 11:20:57 +0800

    Fix inconsistence

--- src/com/android/settings/slim/UserInterface.java
+++ src/com/android/settings/slim/UserInterface.java
@@ -133,7 +133,7 @@ public class UserInterface extends SettingsPreferenceFragment implements
 
         mRecentClearAll = (CheckBoxPreference) prefSet.findPreference(RECENT_MENU_CLEAR_ALL);
         mRecentClearAll.setChecked(Settings.System.getInt(resolver,
-            Settings.System.SHOW_CLEAR_RECENTS_BUTTON, 0) == 1);
+            Settings.System.SHOW_CLEAR_RECENTS_BUTTON, 1) == 1);
         mRecentClearAll.setOnPreferenceChangeListener(this);
 
         mRecentClearAllPosition = (ListPreference) prefSet.findPreference(RECENT_MENU_CLEAR_ALL_LOCATION);
